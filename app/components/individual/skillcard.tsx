412bf9a442446893f7a8fa05f869c18456354970
BryOliveira <jerryrules2004slime@gmail.com>
2025-07-29 23:04:21 -0400

    I.. don't know

---
+++ app/components/individual/skillcard.tsx
@@ -0,0 +1,7 @@
+export default function SkillCard() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import About from './components/about';
+import Skills from './components/skills';
+import Projects from './components/projects';
+import Education from './components/education';
+import Experience from './components/experience';
+
+export default function Page() {
+  return (
+    <div>
+      <About />
+      <Projects />
+      <Skills />
+      <Education />
+      <Experience />
+    </div>
+  );
+}
